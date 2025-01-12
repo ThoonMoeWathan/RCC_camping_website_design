2dc874d0567b8b0353a436be8e9993429bf4c9cc
Thoon Moe Wathan <thoonmoewathan2004@gmail.com>
2025-01-12 20:49:53 +0630

    First Commit

---
+++ css/script.js
@@ -0,0 +1,27 @@
+const hamburger = document.querySelector('.hamburger');
+const hamburger_icon = hamburger.querySelector('span');
+const mobile_menu = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger_icon.innerText = hamburger_icon.innerText === 'menu' 
+        ? 'close'
+        : 'menu';
+
+    mobile_menu.classList.toggle('is-open');
+})
+        const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
